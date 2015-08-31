1116c228b7172eb3613dfc48791f961371814911
MrNicholson85 <darryl.a.nicholson@gmail.com>
2015-08-31 10:03:12 -0700

    top lever styling

--- wp-content/themes/DynamicFitness/header.php
+++ wp-content/themes/DynamicFitness/header.php
@@ -14,6 +14,93 @@
 
 <body>
 
+<div class="top_ledge">
+
+    <div class="container">
+
+        <div class="row">
+
+
+        <div class="col-lg-6">
+
+            <div class="df_contact">
+
+                <div id="df_phone">
+
+                    <span class="glyphicon glyphicon-earphone ledge-glyf white"></span>1-555-555-5555
+
+                </div>
+                <div id="df_contact_form">
+
+                    <div class=" btn-group" role="group">
+
+                        <button type="button" class="btn btn-default dropdown-toggle ledger-btn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+                            <span class="glyphicon glyphicon-envelope white ledge-glyf"></span> Contact 
+
+                        </button>
+
+                        <ul class="dropdown-menu ledger-dd">
+
+                            <div class="df_cont_form">
+
+                                Contact form here
+
+                            </div>
+
+                        </ul>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-lg-6">
+
+            <div class="df_search_login">
+
+                <div class=" btn-group pull-right" role="group">
+
+                    <button type="button" class="btn btn-default dropdown-toggle ledger-btn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+                        <span class="glyphicon glyphicon-user white ledge-glyf"></span> Sign-in
+
+                    </button>
+
+                    <ul class="dropdown-menu ledger-dd">
+
+                        <div class="df_user_login">
+
+                            <?php wp_login_form(); ?>
+
+                        </div>
+
+                    </ul>
+
+                </div>
+
+                <div id="df_search" class="pull-right">
+
+                    <?php get_search_form(); ?>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <span class="clearfix"></span>
+
+
+</div><!-- /.row -->
+
+</div><!--  /.container -->
+
+</div><!--  /.top_ledge -->
+
  <div class="container">
 
     <nav class="navbar navbar-default" role="navigation">
@@ -30,7 +117,6 @@
                                 <img src="<?php bloginfo('template_directory'); ?>/img/logo-lg.png" class="img-responsive" />
 
                             </a>
-
                         </div>
 
                     </div>
@@ -58,7 +144,7 @@
                         'theme_location'    => 'primary',
                         'depth'             => 2,
                         'container'         => 'div',
-                        'container_class'   => 'collapse navbar-collapse pull-right',
+                        'container_class'   => 'collapse navbar-collapse',
                         'container_id'      => 'bs-example-navbar-collapse-1',
                         'menu_class'        => 'nav navbar-nav',
                         'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
@@ -78,14 +164,6 @@
 <div class="pull-right">
                     <a href="#" id="cart"><img src="<?php bloginfo('template_directory'); ?>/img/cart.png" /></a>
                 </div>
-
-                     <form action="" class="search-form pull-right">
-                        <div class="form-group has-feedback ">
-                            <label for="search" class="sr-only">Search</label>
-                            <input type="text" class="form-control" name="search" id="search" placeholder="search">
-                            <span class="glyphicon glyphicon-search form-control-feedback"></span>
-                        </div>
-                    </form>
 
                 </div>
 
